a596d26399a6db231cf42f7d4a3ea3b6d8421d84
RafiArizu <rafiariz95@Gmail.com>
2025-10-24 09:22:49 +0700

    pakai construct

--- luas_lingkaran.php
+++ luas_lingkaran.php
@@ -2,19 +2,45 @@
 
 class LuasLingkaran{
 
-    public int $jari_jari;
     public const PHI = 3.14;
+
+    public int $jari_jari;
+
+
+    public function __construct($JariValue = 1){
+        echo "Kasih Paham BUB! <br>";
+        $this->jari_jari = $JariValue;
+    }
+
+    public function tampil($nama = 'ban'){
+        $rumus = LuasLingkaran::PHI * ($this->jari_jari * $this->jari_jari); // this berfungsi untuk memanggil langsung variabel yang menggunakan function
+        echo "Lingkaran {$nama} Hasilnya Adalah: {$rumus}";
+    }
+
+    public static function testing(){
+        echo"<br/>";
+        echo "Ini Testing Static Woi";
+    }
+
+    public function __destruct(){
+        echo"<br>";
+        echo"Hi Salam Kenal";
+    }
 
 }
 
 $linkgaran = new LuasLingkaran();
+// $linkgaran->jari_jari = 10;
+// $rumus = LuasLingkaran::PHI * ($linkgaran->jari_jari * $linkgaran->jari_jari);
+// echo "Hasilnya Adalah: " . $rumus;
 
-$linkgaran->jari_jari = 10;
 
-$rumus = LuasLingkaran::PHI * ($linkgaran->jari_jari * $linkgaran->jari_jari);
+// cara panggil function
+$linkgaran->tampil("Roda");
 
+// Cara panggil function static
+LuasLingkaran::testing();
 
-echo "Hasilnya Adalah: " . $rumus;
 
 
 
